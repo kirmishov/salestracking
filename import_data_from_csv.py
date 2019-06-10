cc1084cce379bb3bdf35737eb946cd3c43943bdf
Sergei Kirmishov <sopirty@gmail.com>
2019-06-10 21:51:54 +0300

    edited standalone script for import data

--- import_data_from_csv.py
+++ import_data_from_csv.py
@@ -1,13 +1,17 @@
+import os
 import re
 import csv
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
 from sales.models import Sale
 from users.models import CustomUser
 
+outcome_choices = ('Won', 'Lost', 'Reschedule', 'Cancelled')
 
-with open('For import Salestracking - Hananel-June-sample.csv') as f:
+with open('For import Salestracking - All.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             try:
@@ -15,13 +19,13 @@ with open('For import Salestracking - Hananel-June-sample.csv') as f:
             except IndexError:
                 cash_c = 0
             _, created = Sale.objects.get_or_create(
-                # author=row[0],
                 author=CustomUser.objects.get(username=row[0]),
                 date=row[1],
                 full_name_customer=row[2],
                 email_customer=row[3],
                 attended=True if row[4]=='yes' else False,
-                outcome=row[5].title(),
+                outcome=row[5].title() if row[5].title() in outcome_choices else 'Reschedule',
                 cash_collected=cash_c,
-                call_notes=row[7]
+                call_notes=row[7],
+                recording_url=row[8]
                 )
